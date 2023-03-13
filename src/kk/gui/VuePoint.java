79e049a1bb68cce540704d27d441f28c9749d9a0
Daphne <daphlarriv@hotmail.com>
2023-03-13 20:35:53 +0100

    Gui graphe finie

---
+++ src/kk/gui/VuePoint.java
@@ -0,0 +1,57 @@
+
+/**
+ * @author LARRIVAIN Daphne 22112005
+ */
+
+package kk.gui;
+
+import kk.geometrie.Point;
+import kk.geometrie.Segment;
+import kk.gui.Boule;
+
+import java.awt.*; 
+import javax.swing.*;
+import java.util.*;
+
+//-----------------------------------------------//
+
+public class VuePoint extends JPanel{
+	private int DIAM; //Diamètre de la boule et largeur de la vue
+	private int HEIGHT;
+	private Point point;
+	private Boule boule;
+	private JLabel label;
+
+	public VuePoint(Point p, int diam){
+		super(new GridLayout(2, 1));	
+		//super.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		this.DIAM = diam;
+		this.point = p;
+		this.boule = new Boule(DIAM);
+		this.label = new JLabel(p.getValue().toString());
+		
+		this.add(this.boule);
+		this.add(this.label);
+	}
+
+	//-------------------------------------------------//
+	
+	public int x(){
+		return (int) this.point.getX();
+	}
+
+	public int y(){
+		return (int) this.point.getY();
+	}
+	
+	public int diam(){
+		return this.DIAM;
+	}
+	public int height(){
+		return this.DIAM*2;
+	}
+	
+	public ArrayList<Segment> getSeg(){
+		return this.point.getSeg();
+	}
+}
---
+++ src/kk/gui/util/Listenable.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author LARRIVAIN Daphne 22112005
+ */
+
+package kk.gui.util;
+
+//------------------------------------------------------------------//
+
+public interface Listenable{
+  public abstract void addListener(Listener l);
+  public abstract void dropListener(Listener l);
+}
